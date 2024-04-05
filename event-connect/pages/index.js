fb49b6cbf497edd38b6be8d2a3e7fe14b7238d99
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2024-04-05 12:10:00 +0530

    landing page

--- event-connect/pages/index.js
+++ event-connect/pages/index.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from 'react';
+import { FaSearch, FaTimes } from 'react-icons/fa'; 
+//import { FaDancing, FaMusic, FaTheaterMasks, FaMicrophoneAlt,FaSearch, FaTimes } from 'react-icons/fa';
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
+//import styles from "@/styles/Home.module.css";
+//import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+/*export default function Home() {
   return (
     <div className={styles.container}>
       <Link href={'/signup'}>Signup</Link>
@@ -17,4 +20,67 @@ export default function Home() {
 
     </div>
   );
-}
+}*/
+
+
+import Link from 'next/link';
+import styles from '@/styles/HomePage.module.css';
+
+const HomePage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const clearSearch = () => {
+    setSearchTerm('');
+  };
+ 
+    
+
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+      
+        <div className={styles.logo}>
+          <h1>event Connect</h1>
+        </div>
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Search events"value={searchTerm}onChange={handleInputChange} className={styles.searchInput} />
+          {searchTerm && <FaTimes className={`${styles.icon} ${styles.clearIcon}`} onClick={clearSearch} />}
+          {!searchTerm && <FaSearch className={`${styles.icon} ${styles.searchIcon}`} />}
+        </div>
+        <div className={styles.navBar}>
+          <ul>
+            <li>
+              <Link href="/browse-events">Browse Events</Link>
+              
+             
+            </li>
+            <li>
+              <Link href="/create-event">Create Event</Link>
+            </li>
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
+            <li>
+              <Link href="/signup">Signup</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.image}>
+        <img src="img1.jpg" />
+      </div>
+      
+     
+      </div>
+      
+    
+  );
+};
+
+export default HomePage;
